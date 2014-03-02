2c8ae2167aeb20c24514b6b9d2656a317ba0c123
jcap49 <jcapecelatro@gmail.com>
2014-03-02 13:51:28 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Coinery::Application.config.secret_key_base = '9b334f16daecc3f0a37110c489cb24fe19ef4b6b6cad86d9249f998e227074b352a3adeaf0e5f0ac4fc08657dc7d13c5bf4c7ace05346601dd119f3253bd0067'
