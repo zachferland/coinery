ceed99bfc2ef75eecc1d5f61afe74dd6f6c44019
Zach Ferland <zachferland@gmail.com>
2014-03-17 03:41:20 -0400

    coinbase auth refresh

--- app/models/coinbase_authentication.rb
+++ app/models/coinbase_authentication.rb
@@ -10,10 +10,11 @@ class CoinbaseAuthentication < ActiveRecord::Base
 	end
 
 	def refresh
-		if !valid? 
+		# if !valid? 
+		# force refresh each product creation for now, a bit janky
 			post_string = 'https://coinbase.com/oauth/token?grant_type=refresh_token&code=' + coinbase_user_id + '&refresh_token=' + refresh_token
 			response = HTTParty.post(post_string).parsed_response
 			update(access_token: response['access_token'], refresh_token: response['refresh_token'], expires_at: response['expires_at'])
-		end 
+		# end 
 	end 
 end
